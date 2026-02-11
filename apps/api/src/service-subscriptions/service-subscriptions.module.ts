c93807ccb4585d4b5000b7b5f5ebfb13e72de1fa
alexander flores <alexfloresrayme@gmail.com>
2026-02-11 18:44:05 -0500

    update:API dev script, port and native config

--- apps/api/src/service-subscriptions/service-subscriptions.module.ts
+++ apps/api/src/service-subscriptions/service-subscriptions.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ServiceSubscriptionsService } from './service-subscriptions.service';
 import { ServiceSubscriptionsController } from './service-subscriptions.controller';
-import { UsersModule } from '@/users/users.module';
 
 @Module({
-  imports: [UsersModule],
   controllers: [ServiceSubscriptionsController],
   providers: [ServiceSubscriptionsService],
 })
