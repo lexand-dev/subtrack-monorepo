726d5e769cb9e94f3d3f39255488f1ac2356bdb5
alexander flores <alexfloresrayme@gmail.com>
2026-02-13 23:54:19 -0500

    remove:service-subscriptions module and files

--- apps/api/src/service-subscriptions/service-subscriptions.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ServiceSubscriptionsService } from './service-subscriptions.service';
-
-@Controller('service-subscriptions')
-export class ServiceSubscriptionsController {
-  constructor(private readonly serviceSubscriptionsService: ServiceSubscriptionsService) {}
-}
--- apps/api/src/service-subscriptions/service-subscriptions.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ServiceSubscriptionsService } from './service-subscriptions.service';
-import { ServiceSubscriptionsController } from './service-subscriptions.controller';
-
-@Module({
-  controllers: [ServiceSubscriptionsController],
-  providers: [ServiceSubscriptionsService],
-})
-export class ServiceSubscriptionsModule {}
